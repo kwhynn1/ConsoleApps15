2116c665c3ef7ec1fe830258aeda62e0b808a062
kwhynn1 <91710013+kwhynn1@users.noreply.github.com>
2022-02-07 03:34:07 +0000

    Initial commit

---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleAppProject.App03;
+using ConsoleAppProject.Helpers;
+using System;
+
+namespace ConsoleAppProject
+{
+    /// <summary>
+    /// The main method in this class is called first
+    /// when the application is started.  It will be used
+    /// to start App01 to App05 for CO453 CW1
+    /// 
+    /// This Project has been modified by:
+    /// Derek Peacock 05/02/2022
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine();
+            Console.WriteLine(" =================================================");
+            Console.WriteLine("    BNU CO453 Applications Programming 2021-2022! ");
+            Console.WriteLine("        by Derek Peacock and Nicholas Day         ");
+            Console.WriteLine(" =================================================");
+            Console.WriteLine();
+        }
+    }
+}
