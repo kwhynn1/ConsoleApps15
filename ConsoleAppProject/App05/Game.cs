3054f7354fecd30e3c564cfa457c158190e4f2fd
kwhynn1 <91710013+kwhynn1@users.noreply.github.com>
2022-04-15 14:10:08 +0100

    game base

---
+++ ConsoleAppProject/App05/Game.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ConsoleAppProject.App05
+{
+    public class Game
+    {
+
+        public const int Max_Rounds = 3;
+
+        public int Round;
+
+        public Player Human;
+
+        public Player Computer;
+
+        public Player CurrentPlayer;
+        
+        public Player Winner;
+
+        private Random generator = new Random(55);
+
+        public Game (string name) 
+        {
+            Human = new Player("Peter", Player.GamePlayer.Player);
+            Computer = new Player("Computer", Player.GamePlayer.Computer);
+            Round = 0;
+        }
+
+        public void Start() 
+        {
+            Round = 1;
+            CurrentPlayer = Human;
+            Human.Score = 0;
+            Computer.Score = 0;
+        }
+
+        public void ComputerChoice()
+        {
+            int val = generator.Next(1,4);
+
+            switch (val)
+            {
+                case 1: Computer.Choice = Player.GameChoices.Rock; break;
+                case 2: Computer.Choice = Player.GameChoices.Paper; break;
+                case 3: Computer.Choice = Player.GameChoices.Scissors; break;
+            }
+        }
+
+        public void ScoreRound()
+        {
+            if (Human.Choice == Player.GameChoices.Rock && Computer.Choice == Player.GameChoices.Paper) 
+            {
+                Computer.Score += 2;
+                Winner = Computer;
+
+            }
+
+            if (Round < 3)
+            {
+                Round++;
+            }
+       
+        }
+
+        public void EndGame()
+        {
+            if (Computer.Score > Human.Score)
+            {
+                Winner = Computer;
+            }
+            else if (Computer.Score < Human.Score)
+            {
+                Winner = Human;
+            }
+
+            else Winner = null;
+
+        }
+
+
+    }
+}
---
+++ ConsoleAppProject/App05/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App05
+{
+   public class Player
+    {
+        public enum GameChoices
+        {
+            None,
+            Paper,
+            Rock,
+            Scissors
+        }
+
+        public enum GamePlayer
+        {
+            None,
+            Player,
+            Computer
+        }
+
+        public string Name;
+
+        public int Score;
+
+        public GamePlayer PlayerType;
+
+        public GameChoices Choice;
+
+        public Player(string name, GamePlayer playerType) 
+        { 
+            this.Name = name;
+            this.PlayerType = playerType;
+            Score = 0;
+            Choice = GameChoices.None;
+        }
+
+    }
+}
