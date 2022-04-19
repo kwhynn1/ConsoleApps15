e505ea38fa6e13323cb2499d82c3ddb771bd5ad8
kwhynn1 <91710013+kwhynn1@users.noreply.github.com>
2022-04-19 11:53:29 +0100

    Update Game.cs

--- ConsoleAppProject/App05/Game.cs
+++ ConsoleAppProject/App05/Game.cs
@@ -44,15 +44,15 @@ namespace ConsoleAppProject.App05
 
                 if (choice == "1")
                 {
-                    Player1.Choice = Player.GameChoices.Rock;
+                    player.Choice = Player.GameChoices.Rock;
                 }
                 else if (choice == "2")
                 {
-                    Player1.Choice = Player.GameChoices.Paper;
+                    player.Choice = Player.GameChoices.Paper;
                 }
                 else if (choice == "3")
                 {
-                    Player1.Choice = Player.GameChoices.Scissors;
+                    player.Choice = Player.GameChoices.Scissors;
                 }
                 else
                 {
@@ -62,78 +62,78 @@ namespace ConsoleAppProject.App05
                 }
             }
 
-            if (player.PlayerType == Computer.PlayerType)
+            else if (player.PlayerType == Computer.PlayerType)
             {
                 int val = generator.Next(1, 3);
 
                 switch (val)
                 {
-                    case 1: Computer.Choice = Player.GameChoices.Rock; break;
-                    case 2: Computer.Choice = Player.GameChoices.Paper; break;
-                    case 3: Computer.Choice = Player.GameChoices.Scissors; break;
+                    case 1: player.Choice = Player.GameChoices.Rock; break;
+                    case 2: player.Choice = Player.GameChoices.Paper; break;
+                    case 3: player.Choice = Player.GameChoices.Scissors; break;
                 }
             }
         }
-        public void ScoreRound()
+        public void ScoreRound(Player player, Player player2)
             {
-                if (Player1.Choice == Player.GameChoices.Rock && Computer.Choice == Player.GameChoices.Paper)
+                if (player.Choice == Player.GameChoices.Rock && player2.Choice == Player.GameChoices.Paper)
                 {
-                    Winner = Computer;
-                    Computer.Score += 1;
+                    Winner = player2;
+                    player2.Score += 1;
                     Console.WriteLine();
                     Console.WriteLine($"Win is awarded to {Winner.Name}");
                 }
-                else if (Player1.Choice == Player.GameChoices.Rock && Computer.Choice == Player.GameChoices.Rock)
+                else if (player.Choice == Player.GameChoices.Rock && player2.Choice == Player.GameChoices.Rock)
                 {
                     Console.WriteLine();
                     Console.WriteLine("No Point is awarded for this Round, Both Players Made The Same Move");
                     Winner = null;
                 }
-                else if (Player1.Choice == Player.GameChoices.Rock && Computer.Choice == Player.GameChoices.Scissors)
+                else if (player.Choice == Player.GameChoices.Rock && player2.Choice == Player.GameChoices.Scissors)
                 {
-                    Player1.Score += 1;
-                    Winner = Player1;
+                    player.Score += 1;
+                    Winner = player;
                     Console.WriteLine();
                     Console.WriteLine($"1 Point is awarded to {Winner.Name}");
 
                 }
-                else if (Player1.Choice == Player.GameChoices.Scissors && Computer.Choice == Player.GameChoices.Scissors)
+                else if (player.Choice == Player.GameChoices.Scissors && player2.Choice == Player.GameChoices.Scissors)
                 {
                     Console.WriteLine();
                     Console.WriteLine("No Point is awarded for this Round, Both Players Made The Same Move");
                     Winner = null;
                 }
-                else if (Player1.Choice == Player.GameChoices.Scissors && Computer.Choice == Player.GameChoices.Paper)
+                else if (player.Choice == Player.GameChoices.Scissors && player2.Choice == Player.GameChoices.Paper)
                 {
-                    Player1.Score += 1;
-                    Winner = Player1;
+                    player.Score += 1;
+                    Winner = player;
                     Console.WriteLine();
                     Console.WriteLine($"1 Point is awarded to {Winner.Name}");
                 }
-                else if (Player1.Choice == Player.GameChoices.Scissors && Computer.Choice == Player.GameChoices.Rock)
+                else if (player.Choice == Player.GameChoices.Scissors && player2.Choice == Player.GameChoices.Rock)
                 {
-                    Winner = Computer;
-                    Computer.Score += 1;
+                    Winner = player2;
+                    player2.Score += 1;
                     Console.WriteLine();
                     Console.WriteLine($"Win is awarded to {Winner.Name}");
                 }
-                else if (Player1.Choice == Player.GameChoices.Paper && Computer.Choice == Player.GameChoices.Paper)
+                else if (player.Choice == Player.GameChoices.Paper && player2.Choice == Player.GameChoices.Paper)
                 {
                     Console.WriteLine();
                     Console.WriteLine("No Point is awarded for this Round, Both Players Made The Same Move");
                     Winner = null;
                 }
-                else if (Player1.Choice == Player.GameChoices.Paper && Computer.Choice == Player.GameChoices.Scissors)
+                else if (player.Choice == Player.GameChoices.Paper && player2.Choice == Player.GameChoices.Scissors)
                 {
-                    Winner = Computer;
-                    Computer.Score += 1;
+                    Winner = player2;
+                    player2.Score += 1;
                     Console.WriteLine();
                     Console.WriteLine($"Win is awarded to {Winner.Name}");
                 }
-                else if (Player1.Choice == Player.GameChoices.Paper && Computer.Choice == Player.GameChoices.Rock)
+                else if (player.Choice == Player.GameChoices.Paper && player2.Choice == Player.GameChoices.Rock)
                 {
-                    Player1.Score += 1;
-                    Winner = Player1;
+                    player.Score += 1;
+                    Winner = player;
                     Console.WriteLine();
                     Console.WriteLine($"1 Point is awarded to {Winner.Name}");
                 }
@@ -143,56 +143,92 @@ namespace ConsoleAppProject.App05
                 Console.WriteLine("Enter The Number of Rounds you would like to Play");
                 string input = Console.ReadLine();
                 Rounds = Convert.ToInt32(input);
-                
+
+            if (mode == "single")
+            {
+
                 for (int i = 0; i < Rounds; i++)
                 {
                     CurrentPlayer = Player1;
                     EnterChoice(CurrentPlayer);
                     CurrentPlayer = Computer;
                     EnterChoice(CurrentPlayer);
-                    ScoreRound();
+                    ScoreRound(Player1, Computer);
                     Console.WriteLine();
                     Console.WriteLine($"{Player1.Name}'s Score Is {Player1.Score}");
                     Console.WriteLine();
                     Console.WriteLine($"Computer's Score Is {Computer.Score}");
-                }
+                } 
 
                 if (Player1.Score > Computer.Score)
                 {
-                Console.WriteLine();
-                Console.WriteLine($"The Winner is {Player1.Name}");
-                } 
-                else if (Player1.Score < Computer.Score) 
-                {
-                Console.WriteLine();
-                Console.WriteLine($"The Winner is {Computer.Name}");
+                    Console.WriteLine();
+                    Console.WriteLine($"The Winner is {Player1.Name}");
                 }
+                else if (Player1.Score < Computer.Score)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"The Winner is {Computer.Name}");
+                }
+            }
+            else if (mode == "vs")
+            {
+                for (int i = 0; i < Rounds; i++)
+                {
+                    CurrentPlayer = Player1;
+                    Console.WriteLine();
+                    Console.WriteLine($"It's {Player1.Name} turn");
+                    EnterChoice(CurrentPlayer);
+                    CurrentPlayer = Player2;
+                    Console.WriteLine();
+                    Console.WriteLine($"It's {Player2.Name} turn");
+                    EnterChoice(CurrentPlayer);
+                    ScoreRound(Player1,Player2);
+                    Console.WriteLine();
+                    Console.WriteLine($"{Player1.Name}'s Score Is {Player1.Score}");
+                    Console.WriteLine();
+                    Console.WriteLine($"{Player2.Name}'s Score Is {Player2.Score}");
+                }
+
+                if (Player1.Score > Player2.Score)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"The Winner is {Player1.Name}");
+                }
+                else if (Player1.Score < Player2.Score)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"The Winner is {Player2.Name}");
+                }
+
+            }
         }
         public void Menu ()
         {
-
-            
             string[] choices = new string[]
               {
-                "Player Vs Computer","Quit"
+                "Player Vs Computer","Player vs Player ", "Quit"
               };
             bool Quit = false; ;
-
             do
             {
                 int choice = ConsoleHelper.SelectChoice(choices);
-
                 switch (choice)
                 {
                     case 1:
                         Player1 = EnterPlayerName();
                         Computer = new Player("Computer", Player.GamePlayer.Computer);
                         GameRound("single"); break;
-                    case 2: Quit = true; break;
+                    case 2: Player1 = EnterPlayerName();
+                            Player2 = EnterPlayerName();
+                            GameRound("vs"); break;
+
+                    case 3: Quit = true; break;
                 }
 
             } while (!Quit);
         }
+
     }
 
  }
